58aff012947115fa1955343a9af39d183cfc3cac
Istiaq2064 <arifistiaqcse@gmail.com>
2025-11-05 13:50:52 +0600

    table update form

---
+++ WebApplication/Arif.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class Arif : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var name = Name.Text;
+            var email = Email.Text;
+            var password = Pass.Text;       
+
+            var consString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(consString))
+
+            {
+                string query = "INSERT INTO Istiaq (Name, Email, Password) VALUES (@Name, @Email, @Password)";
+
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                sqlCommand.Parameters.AddWithValue("@Name", name);
+                sqlCommand.Parameters.AddWithValue("@Email", email);
+                sqlCommand.Parameters.AddWithValue("@Password", password);
+
+                con.Open();
+                sqlCommand.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
